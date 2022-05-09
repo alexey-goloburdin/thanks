6045b1d4d1397d4fd05ba183288bb608318d9d28
JIiCeHoK <foxs29546@gmail.com>
2022-05-09 11:01:16 +0500

    Инициализация проекта

---
+++ askme-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                    dsf
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
